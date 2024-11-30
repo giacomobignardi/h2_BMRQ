2659fcbcf8501a81eee9c8183b547a1e1b6e449a
giacomo bignardi <giacomo@bignardi.it>
2024-11-30 14:14:28 +0100

    minimal R packages

--- R/R_packages.R
+++ R/R_packages.R
@@ -1,8 +1,8 @@
 #Description: Install packages in the VDI
-#this script contains all the extra R packages needed to run the analyses (including supplementary analyses)
+#this script contains all the R packages needed to run the main analyses
 #Program: R_packages ------------------------------------------------------------------------------------------------------------------------------
 #set path where to store the packages
-.libPaths('W:\\XXX\04_packages')
+.libPaths('W:\\C4_Behavior_Genectics_Unit\\Giacomo Bignardi\\2023_BMRQ\\04_packages')
 #get packages from KI repo
 options(repos = 'http://nexus.ki.se/repository/cran.r-project.org/')
 install.packages("lavaan", dependencies = TRUE)
@@ -10,10 +10,7 @@ install.packages("tidyverse")
 install.packages("rstatix")
 install.packages("psych")
 install.packages("patchwork")
-install.packages("wesanderson")
-install.packages("OpenMx")
-install.packages("ggnewscale")
-install.packages('umx')
+install.packages("openxlsx")
 
 
 
