5c1c476c1cb27bfc5fddd384f35d61f840414a91
giacomo bignardi <giacomo@bignardi.it>
2024-11-30 14:23:15 +0100

    adapt to revision 1

--- R/02_MRS_phenotypic_cor.R
+++ R/02_MRS_phenotypic_cor.R
@@ -5,7 +5,7 @@
 #
 #Description: Compute observed phenotypic correlations between music discrimination abilities, general reward sensitivity, and music reward sensitivity
 #Program: 02 ------------------------------------------------------------------------------------------------------------------------------
-.libPaths('W:\\XXX\04_packages')
+.libPaths('W:\\C4_Behavior_Genectics_Unit\\Giacomo Bignardi\\2023_BMRQ\\04_packages')
 
 # load packages
 library(tidyverse)
@@ -16,23 +16,24 @@ set.seed(123)
 
 # set Open Access working directories
 wdOA = getwd()
-wdOA_scripts = "02_scripts"
-wdOA_output = "03_outputs"
+wdOA_scripts = "06_github/R"
+wdOA_output = "03_outputs/01_final"
+wdOA_sfile = "06_github/SFILE"
 
 # set not Open Access working directories
 wdNOA_Data = "01_data"
-wdOA_ImageOutput = "05_images"
+wdOA_ImageOutput = "05_images/01_final"
 
 # load dataFrames:
 twin_BMRQ_wide  = read_csv(sprintf("%s/%s/00_twin_BMRQ_wide.csv", wdOA,wdOA_output))
 
 # CROSS-TRAIT CORRELATIONS####
 # twin 1####
-phenotypes_t1 = twin_BMRQ_wide %>% select(music_discrimination_1,BAS_RR_1,BMRS_total_1) %>% rename("musical\nauditory\ndiscr." = music_discrimination_1, "general\nreward\nsensitivty" = BAS_RR_1,"music\nreward\nsensitivty" = BMRS_total_1)
+phenotypes_t1 = twin_BMRQ_wide %>% select(music_discrimination_1,BAS_RR_1,BMRS_total_1) %>% rename("music\nperceptual\nabilities" = music_discrimination_1, "general\nreward\nsensitivty" = BAS_RR_1,"music\nreward\nsensitivty" = BMRS_total_1)
 observed_varCov_T1 =  cor(phenotypes_t1, use = "pairwise.complete.obs")
 
 # compute standard deviations (Sd)
-sd_mda_t1 = sd(phenotypes_t1$`musical\nauditory\ndiscr.`, na.rm = T)
+sd_mda_t1 = sd(phenotypes_t1$`music\nperceptual\nabilities`, na.rm = T)
 sd_grs_t1 = sd(phenotypes_t1$`general\nreward\nsensitivty`, na.rm = T)
 sd_mrs_t1 = sd(phenotypes_t1$`music\nreward\nsensitivty`, na.rm = T)
 
@@ -47,12 +48,12 @@ observed_varCov_T1 = observed_varCov_T1 %>%
 
 # adapted from StackOverlfow https://stackoverflow.com/questions/67530405/change-orientation-of-diagonal-of-correlation-plot-using-ggcorrplot-package-if
 observed_varCov_long_T1 = observed_varCov_T1 %>% 
-  pivot_longer(c("musical\nauditory\ndiscr.": "music\nreward\nsensitivty"),names_to = "colname", values_to = "value" )
+  pivot_longer(c("music\nperceptual\nabilities": "music\nreward\nsensitivty"),names_to = "colname", values_to = "value" )
 
 # convert to factor so that rows and columns have the same order as the data
 observed_varCov_long_T1 = observed_varCov_long_T1 %>% 
-  mutate(rowname = factor(rowname, levels = c("musical\nauditory\ndiscr.", "general\nreward\nsensitivty","music\nreward\nsensitivty")),
-         colname = factor(colname, levels = rev(c("musical\nauditory\ndiscr.", "general\nreward\nsensitivty","music\nreward\nsensitivty"))))
+  mutate(rowname = factor(rowname, levels = c("music\nperceptual\nabilities", "general\nreward\nsensitivty","music\nreward\nsensitivty")),
+         colname = factor(colname, levels = rev(c("music\nperceptual\nabilities", "general\nreward\nsensitivty","music\nreward\nsensitivty"))))
 
 # set diagonal and the top-right half of the matrix to 0 so that those cells appears white
 p_crosstrait_1 = ggplot(observed_varCov_long_T1, aes(x = colname, y = rowname, fill = value)) +
@@ -62,16 +63,16 @@ p_crosstrait_1 = ggplot(observed_varCov_long_T1, aes(x = colname, y = rowname, f
                        limits = c(-0.5,0.5)) +
   geom_text(aes(colname, rowname, label = round(value,2)), color = "black", size = 3.5) +
   theme_minimal(base_size = 12)+
-  labs(x = NULL, y = NULL, subtitle = "",fill =  "cross\nphenotypic r")+
+  labs(x = NULL, y = NULL, fill =  "cross\nphenotypic r")+
   theme(axis.text.x = element_text(angle = 45, hjust=1)) +
   coord_equal(1)
 
 # twin 2####
-phenotypes_T2 = twin_BMRQ_wide %>% select(music_discrimination_2,BAS_RR_2,BMRS_total_2) %>% rename("musical\nauditory\ndiscr." = music_discrimination_2, "general\nreward\nsensitivty" = BAS_RR_2,"music\nreward\nsensitivty" = BMRS_total_2)
+phenotypes_T2 = twin_BMRQ_wide %>% select(music_discrimination_2,BAS_RR_2,BMRS_total_2) %>% rename("music\nperceptual\nabilities" = music_discrimination_2, "general\nreward\nsensitivty" = BAS_RR_2,"music\nreward\nsensitivty" = BMRS_total_2)
 observed_varCov_T2 =  cor(phenotypes_T2, use = "pairwise.complete.obs")
 
 # compute standard deviations (Sd)
-sd_mda_T2 = sd(phenotypes_T2$`musical\nauditory\ndiscr.`, na.rm = T)
+sd_mda_T2 = sd(phenotypes_T2$`music\nperceptual\nabilities`, na.rm = T)
 sd_grs_T2 = sd(phenotypes_T2$`general\nreward\nsensitivty`, na.rm = T)
 sd_mrs_T2 = sd(phenotypes_T2$`music\nreward\nsensitivty`, na.rm = T)
 
@@ -85,12 +86,12 @@ observed_varCov_T2 = observed_varCov_T2 %>%
 
 # adapted from StackOverlfow https://stackoverflow.com/questions/67530405/change-orientation-of-diagonal-of-correlation-plot-using-ggcorrplot-package-if
 observed_varCov_long_T2 = observed_varCov_T2 %>% 
-  pivot_longer(c("musical\nauditory\ndiscr.": "music\nreward\nsensitivty"),names_to = "colname", values_to = "value" )
+  pivot_longer(c("music\nperceptual\nabilities": "music\nreward\nsensitivty"),names_to = "colname", values_to = "value" )
 
 # convert to factor so that rows and columns have the same order as the data
 observed_varCov_long_T2 = observed_varCov_long_T2 %>% 
-  mutate(rowname = factor(rowname, levels = c("musical\nauditory\ndiscr.", "general\nreward\nsensitivty","music\nreward\nsensitivty")),
-         colname = factor(colname, levels = rev(c("musical\nauditory\ndiscr.", "general\nreward\nsensitivty","music\nreward\nsensitivty"))))
+  mutate(rowname = factor(rowname, levels = c("music\nperceptual\nabilities", "general\nreward\nsensitivty","music\nreward\nsensitivty")),
+         colname = factor(colname, levels = rev(c("music\nperceptual\nabilities", "general\nreward\nsensitivty","music\nreward\nsensitivty"))))
 
 # set diagonal and the top-right half of the matrix to 0 so that those cells appears white
 p_crosstrait_2 = ggplot(observed_varCov_long_T2, aes(x = colname, y = rowname, fill = value)) +
@@ -100,22 +101,13 @@ p_crosstrait_2 = ggplot(observed_varCov_long_T2, aes(x = colname, y = rowname, f
                        limits = c(-0.5,0.5)) +
   geom_text(aes(colname, rowname, label = round(value,2)), color = "black", size = 3.5) +
   theme_minimal()+
-  labs(x = NULL, y = NULL, subtitle = "",fill = "cross\nphenotypic r")+
+  labs(x = NULL, y = NULL, fill = "cross\nphenotypic r")+
   theme(axis.text.x = element_text(angle = 45, hjust=1)) +
   coord_equal(1)
 
 #SAVE####
-save(p_crosstrait_1,file = sprintf("%s/%s/02_pheno_cor_T1.Rdata",wdOA,wdOA_output))
-save(p_crosstrait_2,file = sprintf("%s/%s/02_pheno_cor_T2.Rdata",wdOA,wdOA_output))
-
-pdf(sprintf("%s/%s/02_pheno_cor_T1.pdf",wdOA,wdOA_ImageOutput),
-    width =4,
+pdf(sprintf("%s/%s/02_pheno_cor.pdf",wdOA,wdOA_ImageOutput),
+    width =7,
     height =3.5)
-p_crosstrait_1
-dev.off()
-
-pdf(sprintf("%s/%s/02_pheno_cor_T2.pdf",wdOA,wdOA_ImageOutput),
-    width =4,
-    height =3.5)
-p_crosstrait_2
+(p_crosstrait_1 + theme(legend.position = "none")|p_crosstrait_2) + plot_layout(guides ="collect") + plot_annotation(tag_levels = "a")
 dev.off()
