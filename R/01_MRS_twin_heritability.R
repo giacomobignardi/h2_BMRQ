e02f4bc02fd63b4378a734da20485956cfa8dd1f
giacomo bignardi <giacomo@bignardi.it>
2024-11-30 14:36:27 +0100

    adpat to revision 1

--- R/01_MRS_twin_heritability.R
+++ R/01_MRS_twin_heritability.R
@@ -5,7 +5,7 @@
 #
 #Description: Strucutral Equation Modeling Classical Twin Design specification
 #Program: 01------------------------------------------------------------------------------------------------------------------------------
-.libPaths('W:\\XXX\04_packages')
+.libPaths('W:\\C4_Behavior_Genectics_Unit\\Giacomo Bignardi\\2023_BMRQ\\04_packages')
 
 # load packages
 library(tidyverse)
@@ -18,12 +18,13 @@ set.seed(123)
 
 # set Open Access working directories
 wdOA = getwd()
-wdOA_scripts = "02_scripts"
-wdOA_output = "03_outputs"
+wdOA_scripts = "06_github/R"
+wdOA_output = "03_outputs/01_final"
+wdOA_sfile = "06_github/SFILE"
 
 # set not Open Access working directories
 wdNOA_Data = "01_data"
-wdOA_ImageOutput = "05_images"
+wdOA_ImageOutput = "05_images/01_final"
 
 # load dataFrames:
 twin_BMRQ_wide  = read_csv(sprintf("%s/%s/00_twin_BMRQ_wide.csv", wdOA,wdOA_output))
@@ -38,7 +39,7 @@ scatter_plot = twin_BMRQ_wide %>%
     `4` = "DZm",
     `5` = "DZos",
   )) %>% 
-ggplot(aes(BMRS_total_1,BMRS_total_2)) + 
+  ggplot(aes(BMRS_total_1,BMRS_total_2)) + 
   geom_point() + 
   geom_smooth(method = "lm") +
   facet_grid(cols = vars(zyg)) +
@@ -56,7 +57,7 @@ dev.off()
 
 # SATURATED MODEL####
 # source SAT model specification
-source(sprintf("%s/%s/functions/lavaantwda/CTD_SAT_5g_univariate_lavaan.R", wdOA,wdOA_scripts))
+source(sprintf("%s/%s/functions/lavaantwda/SAT_5g_univariate.R", wdOA,wdOA_scripts))
 
 # rename to meet lavaan custom code requirements
 twin_BMRQ_wide = twin_BMRQ_wide %>% 
@@ -70,28 +71,30 @@ nrow(twin_BMRQ_wide)
 
 # fit saturated model with age as covariate
 qSAT_fit = sem(qSAT_mod,
-              data = twin_BMRQ_wide,
-              group = "zyg",
-              group.label= c(1,2,3,4,5),
-              missing = "ML"
-              )
+               data = twin_BMRQ_wide,
+               group = "zyg",
+               group.label= c(1,2,3,4,5),
+               missing = "ML"
+)
 
 # sanity check: group order is respected
 qSAT_fit@Data@group.label
 
 # summary saturated model
-qsumy_SAT_fit = summary(qSAT_fit,ci = T, fit.measures = T)
+qSAT_sumy = summary(qSAT_fit,ci = T, fit.measures = T)
 
 # fit indicies (note that the covariate age is manifests, so sat is partially constrained)
 fitmeasures(qSAT_fit,c("cfi","srmr"))
+#cfi  srmr 
+#1.000 0.008 
 
 # extract standardized covariances (cor) for twin pairs
-sumy_SATstd_fit = standardizedsolution(qSAT_fit)
-sumy_SATstd_fit %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_f_mz")
-sumy_SATstd_fit %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_m_mz")
-sumy_SATstd_fit %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_f_dz")
-sumy_SATstd_fit %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_m_dz")
-sumy_SATstd_fit %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_dos")
+qSAT_std = standardizedsolution(qSAT_fit)
+qSAT_std %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_f_mz")
+qSAT_std %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_m_mz")
+qSAT_std %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_f_dz")
+qSAT_std %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_m_dz")
+qSAT_std %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_dos")
 
 # model check (info on test are in the CTD_SAT_5g_lavaan.R script)
 SAT_fit = sem(SAT_mod, data = twin_BMRQ_wide, group = "zyg", group.label= c(1,2,3,4,5), missing = "ML")
@@ -105,6 +108,9 @@ SAT_fit_qnt_6 = sem(SAT_mod_qnt_6, data = twin_BMRQ_wide, group = "zyg", group.l
 SAT_fit_qal_7 = sem(SAT_mod_qal_7, data = twin_BMRQ_wide, group = "zyg", group.label= c(1,2,3,4,5), missing = "ML")
 
 ## model comparison####
+# saturated vs baseline: reminder, here we constrain age var across groups to ease interpretations of standardized variances across groups
+lavTestLRT(SAT_fit,qSAT_fit)
+
 rbind(
   lavTestLRT(qSAT_fit,SAT_fit_bor_2),
   lavTestLRT(qSAT_fit,SAT_fit_zyg_3),
@@ -114,28 +120,48 @@ rbind(
   lavTestLRT(qSAT_fit,SAT_fit_qal_7)
 )
 
+# report test against saturated (included already in lavaan output "Model Test User Model")
+SAT_fit_bor_2
+SAT_fit_zyg_3
+SAT_fit_sxm_4
+SAT_fit_sxv_5
+SAT_fit_qnt_6
+SAT_fit_qal_7
+
+# test for covariate age (LRT addition)
+## equality of paths
+lavTestLRT(SAT_fit_cov_0,SAT_fit_cov_1)
+
 # summary of model after testing for assumptions
-sumy_SATstd_final_fit = standardizedsolution(SAT_fit_qal_7)
-sumy_SAT_final_fit = summary(SAT_fit_qal_7, ci = T)
+SAT_final_std = standardizedsolution(SAT_fit_qal_7)
+SAT_final_sumy = summary(SAT_fit_qal_7, ci = T)
 summary(qSAT_fit)
 
 # means
-sumy_SAT_final_fit$pe %>% filter(label == "mean_f")
-sumy_SAT_final_fit$pe %>% filter(label == "mean_m")
+SAT_final_sumy$pe %>% filter(label == "mean_f")
+SAT_final_sumy$pe %>% filter(label == "mean_m")
+
+# Coehn s d
+SAT_final_sumy$pe %>% filter(label == "d")
+# alternative d (uncostrained variances)
+SAT_zyg_3_sumy = summary(SAT_fit_zyg_3)
+SAT_zyg_3_sumy$pe %>% filter(label == "d") # same to the rounding
 
 # standardized b1
-sumy_SATstd_final_fit %>% filter(label == "b1")
+SAT_final_std %>% filter(label == "b1")
+# unstandardized b1
+SAT_final_sumy$pe %>% filter(label == "b1")
 
 # which variance component do we expect? if rMZ is twice higher than rDZ then ADE, otherwise ACE
-rMZ = sumy_SATstd_final_fit %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_mz") %>% slice(1)
-rDZ = sumy_SATstd_final_fit %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_dz") %>% slice(1)
+rMZ = SAT_final_std %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_mz") %>% slice(1)
+rDZ = SAT_final_std %>% select(label,est.std,ci.lower,ci.upper) %>% filter(label == "cov_dz") %>% slice(1)
 expected  = ifelse(rMZ$est.std > 2*rDZ$est.std, "ADE", "ACE")
 # We expect A(D)E model
 
 # ADE MODEL####
 # #ADE model####
 # source ADE model specification
-source(sprintf("%s/%s/functions/lavaantwda/CTD_ADE_5g_univariate_lavaan.R", wdOA,wdOA_scripts))
+source(sprintf("%s/%s/functions/lavaantwda/ADE_5g_univariate.R", wdOA,wdOA_scripts))
 
 # fit ADE model with age as covariate
 ADE_fit = sem(ADE_model,
@@ -149,8 +175,8 @@ ADE_fit = sem(ADE_model,
 SAT_fit@Data@group.label
 
 # summary ADE model
-sumy_ADE_fit = summary(ADE_fit,ci = T, fit.measures = T)
-sumy_ADE_fit
+ADE_sumy = summary(ADE_fit,ci = T, fit.measures = T)
+ADE_sumy
 
 ## AE model####
 # fit parsimonious models by dropping D and E
@@ -163,29 +189,33 @@ rbind(
   lavTestLRT(ADE_fit,AE_fit),
   lavTestLRT(AE_fit,E_fit)
 )
+
 fitMeasures(ADE_fit,c("cfi","srmr"))
+#0.983 0.041
+
 fitMeasures(AE_fit,c("cfi","srmr"))
+#0.981 0.042
 
 # summary statistics final model
-sumy_AE_fit = summary(AE_fit,ci = T, fit.measures = T)
+AE_sumy = summary(AE_fit,ci = T, fit.measures = T)
 
 # tidy results ADE
-ADE_ADE_fit = sumy_ADE_fit$pe %>%
+ADE = ADE_sumy$pe %>%
   filter(label == "A" | label == "D" | label == "E") %>% 
   select(label, est) %>% 
   mutate(model = "ADE",
          component = factor(label, levels = c("E", "D", "A")))
 
 # tidy results AE
-ADE_AE_fit = sumy_AE_fit$pe %>% 
+AE = AE_sumy$pe %>% 
   filter(label == "A" | label == "E") %>% 
   add_row(label = "D") %>% select(label, est) %>% 
   mutate(model = "AE",
          component = factor(label, levels = c("E", "D", "A")))
 
-# Supplementary file 5 ####
+# Supplementary file 1 ####
 # prepare table for publication
-SFile_5 = sumy_AE_fit$pe %>% 
+SFile_1 = AE_sumy$pe %>% 
   distinct(label, .keep_all = T) %>% 
   select(label, est, se, pvalue, ci.lower,ci.upper) %>% 
   # remove path equal 1
@@ -201,84 +231,34 @@ SFile_5 = sumy_AE_fit$pe %>%
                         "var_age" =  "σ2age",
                         "varA" =  "σ2A",
                         "varE" =  "σ2E",
-                        )) %>% 
+  )) %>% 
   rename(parameter = label,
          p = pvalue) %>% 
   mutate(across(where(is.numeric), round, 3))
 
-write_csv(SFile_5,sprintf("%s/%s/01_SFILE5.csv", wdOA,wdOA_output))
+write_csv(SFile_1,sprintf("%s/%s/01_supplementary_data_1.csv", wdOA,wdOA_sfile))
 
 # VIZUALIZE####
 ## phenotypic correlations####
+qSAT_std = standardizedsolution(qSAT_fit)
 # extract correlations and confidence intervals from saturated model with covariate age
-pheno_cor = sumy_SATstd_fit %>% select(label,est.std,ci.lower,ci.upper) %>% 
+pheno_cor = qSAT_std  %>% select(label,est.std,ci.lower,ci.upper) %>% 
   mutate(label = fct_recode(label,   
-                              MZf = "cov_f_mz",
-                              MZm = "cov_m_mz",
-                              DZf = "cov_f_dz",
-                              DZm = "cov_m_dz",
-                              DZos= "cov_dos")) %>% 
+                            MZf = "cov_f_mz",
+                            MZm = "cov_m_mz",
+                            DZf = "cov_f_dz",
+                            DZm = "cov_m_dz",
+                            DZos= "cov_dos")) %>% 
   mutate(zygosity = factor(label, levels = rev(c("MZf","MZm","DZf","DZm","DZos"))))
 
 # save for external plotting
-write_csv(pheno_cor,sprintf("%s/%s/01_FIG1B_forestplot_data.csv", wdOA,wdOA_output))
-
-# plot phenotypic correlations and confidence intervals stratified by zygosity
-forest_like_plot = pheno_cor %>% 
-  filter(zygosity %in% c("MZf", "MZm", "DZf", "DZm", "DZos")) %>% 
-  ggplot(aes(est.std,zygosity)) +
-  geom_point(position = position_dodge(.9), size = 2.5)+#3182BD
-  geom_errorbar(aes(xmin = ci.lower, xmax = ci.upper), width = .1, size = 1,position = position_dodge(.9)) +
-  xlim(-0.01,1) +
-  geom_vline(xintercept = 0, linetype = "dashed")+
-  scale_color_viridis_d()+
-  labs(
-    y = "zygosity",
-    x = "phenotypic r"
-    )+
-  theme_classic(12)+
-  theme(axis.text.x = element_text(angle = 45, hjust=1), legend.position = "none")
+write_csv(pheno_cor,sprintf("%s/%s/01_FIG1_forestplot_data.csv", wdOA,wdOA_output))
 
 ## A(D)E model estimates####
-ADE_estimates = rbind(ADE_ADE_fit,ADE_AE_fit)
+ADE_estimates = rbind(ADE,AE)
 
 # save for external plotting
-write_csv(ADE_estimates,sprintf("%s/%s/01_FIG1C_barplot_data.csv", wdOA,wdOA_output))
-
-# plot ADE and AE model estimates
-bar_plot = rbind(ADE_ADE_fit,ADE_AE_fit) %>% 
-  mutate(model = factor(model, levels = c("ADE", "AE"))) %>%
-  rename(estimate = est) %>% 
-  ggplot(aes(model,estimate, fill = component)) +
-  geom_bar(stat = "identity", width = .5, color = "black") +
-  scale_fill_manual(values = c("#5bbcd6","#B19D61" ,"#f98400" ))+
-  geom_text(aes(x=c(1,1,1,2,2,2), y = c(.21,.28,.3,.27,0,.50), label=round(estimate,2)), 
-            color="white", position = "stack")+
-  labs(x = "model",
-       y="proportion of variance")+
-  scale_y_continuous(breaks = round(seq(0, 1, by = 0.25),2), expand = c(0.001,0.001)) +
-  theme_minimal()+
-  theme(axis.ticks.x = element_blank())
-
-#SAVE####
-pdf(sprintf("%s/%s/01_twin_correlations_BMRQ.pdf",wdOA,wdOA_ImageOutput),
-    width =3.5,
-    height =2.75)
-forest_like_plot
-dev.off()
-
-pdf(sprintf("%s/%s/01_ADE_estimates_BMRQ.pdf",wdOA,wdOA_ImageOutput),
-    width =4.4,
-    height =2.75)
-bar_plot
-dev.off()
-
-#F1bc
-pdf(sprintf("%s/%s/01_Fig1bc_ADE_BMRQ.pdf",wdOA,wdOA_ImageOutput),
-    width =4,
-    height =5)
-forest_like_plot/bar_plot
-dev.off()
+write_csv(ADE_estimates,sprintf("%s/%s/01_FIG1_barplot_data.csv", wdOA,wdOA_output))
 
 ## Alternative estimator####
 ### MLR####
@@ -294,6 +274,8 @@ qSAT_fit_MLR = sem(qSAT_mod,
 
 # fit indicies (note that the covariate age is manifests, so sat is partially constrained)
 fitmeasures(qSAT_fit_MLR,c("cfi","srmr"))
+#cfi  srmr 
+#1.000 0.008 
 
 # model check (info on test are in the CTD_SAT_5g_lavaan.R script)
 SAT_fit_sxm_4_MLR = sem(SAT_mod_sxm_4, data = twin_BMRQ_wide, group = "zyg", group.label= c(1,2,3,4,5), missing = "ML", estimator ="MLR")
@@ -303,13 +285,11 @@ lavTestLRT(qSAT_fit,SAT_fit_sxm_4)
 lavTestLRT(qSAT_fit_MLR,SAT_fit_sxm_4_MLR)
 
 # parameter: age
-SAT_fit_qal_7_MLR = sem(SAT_mod_qal_7, data = twin_BMRQ_wide, group = "zyg", group.label= c(1,2,3,4,5), missing = "ML", estimator = "MLR")
-sumy_SATstd_final_fit_MLR = standardizedsolution(SAT_fit_qal_7_MLR)
+SAT_fit_cov_0_MLR = sem(SAT_fit_cov_0, data = twin_BMRQ_wide, group = "zyg", group.label= c(1,2,3,4,5), missing = "ML", estimator = "MLR")
+SAT_fit_cov_1_MLR = sem(SAT_mod_cov_1, data = twin_BMRQ_wide, group = "zyg", group.label= c(1,2,3,4,5), missing = "ML", estimator = "MLR")
 
-# standardized b1
-sumy_SATstd_final_fit_MLR %>% filter(label == "b1")
-sumy_SATstd_final_fit %>% filter(label == "b1")
-
+## equality of paths
+lavTestLRT(SAT_fit_cov_0_MLR,SAT_fit_cov_1_MLR)
 
 # AE model
 # fit parsimonious models by dropping D and E
@@ -318,8 +298,3 @@ E_fit_MLR = sem(E_model,data = twin_BMRQ_wide,group = "zyg",group.label= c(1,2,3
 
 ## model comparison####
 lavTestLRT(AE_fit_MLR,E_fit_MLR)
-
-fitMeasures(ADE_fit,c("cfi","srmr"))
-fitMeasures(AE_fit,c("cfi","srmr"))
-
-
